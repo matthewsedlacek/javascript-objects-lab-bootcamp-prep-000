36f0ca242d5c719f2ce14c08936455d757f1150e
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-02-25 06:43:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
 function deleteFromObjectByKey(object,key) {
   var newObj = Object.assign({},object);
   newObj;
-  delete newObj.key;
+  delete newObj[key];
   newObj;
   object;
 }
 
-function deleteFromObjectBy
